c81f34e0adebc0b2d246eb7979b2f7120fd4c4e3
Mohammad Hossein Heydari <Heydari.mason@gmail.com>
2025-05-03 15:30:27 -0400

    Add WorldTemperatureMap component with temperature CSV and geoJSON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import TemperaturePieChart from "./TemperaturePieChart";
+import WorldTemperatureMap from "./WorldTemperatureMap/WorldTemperatureMap";
 
 function App() {
   return (
     <div className="App">
-      <TemperaturePieChart />
+      <h1>Climate Visualizations</h1>
+
+      <section>
+        <h2>Pie Chart</h2>
+        <TemperaturePieChart />
+      </section>
+
+      <section style={{ marginTop: "40px" }}>
+        <h2>World Temperature Map</h2>
+        <WorldTemperatureMap />
+      </section>
     </div>
   );
 }
